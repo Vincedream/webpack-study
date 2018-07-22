67dae5e056ac909154da338bda52832390f92250
jingchuan <huajinbo@dxy.cn>
2018-07-22 18:12:33 +0800

    feat: 加入dev-server与HMR

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-    require('./index.css')
-    document.write('xixixi')
+require('./index.css')
+console.log('122214444111111xi333323xix444222i')
+
+if (module.hot) {
+    // 实现热更新
+    module.hot.accept();
+}
