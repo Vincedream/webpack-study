40b06f245068ff772b5773d451dddbfd93feb05d
jingchuan <huajinbo@dxy.cn>
2018-08-04 18:04:48 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-require('./index.css')
-console.log('122214444111111xi333323xix444222i')
+require('./index.css');
+
+console.log('122214444111111xi333323xix444222i');
 
 if (module.hot) {
-    // 实现热更新
-    module.hot.accept();
+  // 实现热更新
+  module.hot.accept();
 }
