8c80f8f76db53d6d2692fee102866fbd95071355
jingchuan <huajinbo@dxy.cn>
2018-08-05 14:58:40 +0800

    feat: Babel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-require('./index.css')
+import './index.css'
+import './test.js'
 
-// console.log('122214444111111xi333323xix444222i');
-// const a = 999;
 console.log('222')
+class App {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+}
+const a1 = new App('vince', 15)
+console.log(a1.age)
+async function test() {
+  console.log('async')
+}
+test()
 if (module.hot) {
   // 实现热更新
   module.hot.accept()
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,62 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env, argv) => {
-    const devMode = argv.mode === 'development'
-    return {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'js/bundle.js'
+  const devMode = argv.mode === 'development'
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/bundle.js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[hash].css',
+        chunkFilename: 'css/[id].[hash].css',
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './src/index.html',
-                filename: 'index.html'
-            }),
-            new MiniCssExtractPlugin({
-                filename: "css/[name].[hash].css",
-                chunkFilename: "css/[id].[hash].css",
-            }),
-            new webpack.HotModuleReplacementPlugin(),
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.css/,
-                    use: [
-                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        'css-loader'
-                    ]
-                },
-                {
-                    test: /\.(png|jpg|gif|jpeg|svg)$/,
-                    use: [
-                        {
-                            loader: 'url-loader',
-                            options: {
-                                limit: 1024,
-                                name: '[name].[hash:6].[ext]',
-                                outputPath: 'images/',
-                                publicPath: '../images'
-                            }
-                        }
-                    ]
-                }
-            ]
+        {
+          test: /\.css/,
+          use: [
+            devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+          ],
         },
-        devServer: {
-            compress: true,
-            port: 9000,
-            hot: true,
-            open: true
-        }
-    }
+        {
+          test: /\.(png|jpg|gif|jpeg|svg)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 1024,
+                name: '[name].[hash:6].[ext]',
+                outputPath: 'images/',
+                publicPath: '../images',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devServer: {
+      compress: true,
+      port: 9000,
+      hot: true,
+      open: true,
+    },
+  }
 }
