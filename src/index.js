a67ff59673b39b3d3ce7794165625e97f22a05bc
Vince <3161141062@qq.com>
2019-04-06 00:30:54 +0800

    Revert "Chapter 03"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 require('./index.css')
+console.log('122214444111111xi333323xix444222i')
 
-// console.log('122214444111111xi333323xix444222i');
-// const a = 999;
-console.log('222')
 if (module.hot) {
-  // 实现热更新
-  module.hot.accept()
+    // 实现热更新
+    module.hot.accept();
 }
